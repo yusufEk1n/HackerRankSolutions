70b4f8dd3282b07bda84279eb40ba5cabf3bd0dc
yusufEk1n <ekinn.yusuf56@outlook.com>
2023-03-06 02:05:43 +0300

    Added CompareTheTriplets

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 This repo contains my hackerrank solutions.
 
 ### List of C# solutions.
+- [Solve My First](https://github.com/yusufEk1n/HackerRankSolutions/tree/master/Solutions/Algorithms/SolveMyFirst)
+- [Simple Array Sum](https://github.com/yusufEk1n/HackerRankSolutions/tree/master/Solutions/Algorithms/SimpleArraySum)
---
+++ Solutions/Algorithms/CompareTheTriplets/Result.cs
@@ -0,0 +1,47 @@
+﻿using static System.Console;
+
+class Result
+{
+
+    /*
+     * Complete the 'compareTriplets' function below.
+     *
+     * The function is expected to return an INTEGER_ARRAY.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY a
+     *  2. INTEGER_ARRAY b
+     */
+
+    public static List<int> compareTriplets(List<int> a, List<int> b)
+    {
+        List<int> result = new List<int>() {0, 0};
+        
+        for(int i = 0; i < 3; i++)
+        {
+            if(a[i] > b[i])
+            {
+                result[0]++;
+            }
+            else if(a[i] < b[i])
+            {
+                result[1]++;
+            }
+        }
+        return result;       
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        List<int> a = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(aTemp => Convert.ToInt32(aTemp)).ToList();
+
+        List<int> b = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(bTemp => Convert.ToInt32(bTemp)).ToList();
+
+        List<int> result = Result.compareTriplets(a, b);
+
+        WriteLine(String.Join(" ", result));
+    }
+}
